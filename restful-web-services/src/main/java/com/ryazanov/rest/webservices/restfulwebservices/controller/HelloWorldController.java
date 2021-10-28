c0474a5763bb0e687a0995ddc47a948832f1cf0a
DRyazanov <4PsuUhyz>
2021-10-28 21:13:39 +0300

    HelloWorldBean

--- restful-web-services/src/main/java/com/ryazanov/rest/webservices/restfulwebservices/controller/HelloWorldController.java
+++ restful-web-services/src/main/java/com/ryazanov/rest/webservices/restfulwebservices/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.ryazanov.rest.webservices.restfulwebservices.controller;
 
+import com.ryazanov.rest.webservices.restfulwebservices.model.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,10 @@ public class HelloWorldController {
     @GetMapping("/hello-world")
     public String helloWorld() {
         return "Hello World";
+    }
+
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
     }
 }
---
+++ restful-web-services/src/main/java/com/ryazanov/rest/webservices/restfulwebservices/model/HelloWorldBean.java
@@ -0,0 +1,10 @@
+package com.ryazanov.rest.webservices.restfulwebservices.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class HelloWorldBean {
+    private String message;
+}
