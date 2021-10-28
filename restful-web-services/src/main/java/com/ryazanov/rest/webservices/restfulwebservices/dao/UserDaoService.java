2a68ae75f3795b944ae1511a525de0d018ce5995
DRyazanov <4PsuUhyz>
2021-10-28 21:30:16 +0300

    Simple User dao

---
+++ restful-web-services/src/main/java/com/ryazanov/rest/webservices/restfulwebservices/dao/UserDaoService.java
@@ -0,0 +1,44 @@
+package com.ryazanov.rest.webservices.restfulwebservices.dao;
+
+import com.ryazanov.rest.webservices.restfulwebservices.entity.User;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class UserDaoService {
+    private static List<User> users = new ArrayList<>();
+    private static int usersId = 3;
+
+    static {
+        users.add(new User(1, "Adam", new Date()));
+        users.add(new User(2, "Eve", new Date()));
+        users.add(new User(3, "Jack", new Date()));
+    }
+
+    public List<User> findAll() {
+        return users;
+    }
+
+    public User save(User user) {
+        if (user.getId() == null) {
+            user.setId(++usersId);
+        }
+
+        users.add(user);
+
+        return user;
+    }
+
+    public User find(int id) {
+        for (User user : users) {
+            if (user.getId() == id) {
+                return user;
+            }
+        }
+
+        return null;
+    }
+}
