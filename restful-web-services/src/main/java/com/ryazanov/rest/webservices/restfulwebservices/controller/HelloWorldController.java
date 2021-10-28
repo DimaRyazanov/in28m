d0622e4e40ac6f48e10742f49c5b35c082db1885
DRyazanov <4PsuUhyz>
2021-10-28 21:20:24 +0300

    HelloWorld PathVariable

--- restful-web-services/src/main/java/com/ryazanov/rest/webservices/restfulwebservices/controller/HelloWorldController.java
+++ restful-web-services/src/main/java/com/ryazanov/rest/webservices/restfulwebservices/controller/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.ryazanov.rest.webservices.restfulwebservices.controller;
 
 import com.ryazanov.rest.webservices.restfulwebservices.model.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,5 +16,10 @@ public class HelloWorldController {
     @GetMapping("/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping("/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 }
