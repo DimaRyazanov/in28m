c039a1066aa6668a29b7b1ab982576924d0eba28
DRyazanov <4PsuUhyz>
2021-11-14 00:11:49 +0300

    i18n polish

--- restful-web-services/src/main/java/com/ryazanov/rest/webservices/restfulwebservices/controller/HelloWorldController.java
+++ restful-web-services/src/main/java/com/ryazanov/rest/webservices/restfulwebservices/controller/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.ryazanov.rest.webservices.restfulwebservices.controller;
 import com.ryazanov.rest.webservices.restfulwebservices.model.HelloWorldBean;
 import lombok.AllArgsConstructor;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -32,7 +33,7 @@ public class HelloWorldController {
     }
 
     @GetMapping("/hello-world-internationalized")
-    public String helloWorldInternationalized(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
-        return messageSource.getMessage("good.morning.message", null, locale);
+    public String helloWorldInternationalized() {
+        return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
     }
 }
