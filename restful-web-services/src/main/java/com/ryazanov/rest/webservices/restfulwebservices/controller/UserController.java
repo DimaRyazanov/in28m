9c115f035a8df6dc4dfeac92d542bbf22b62f78a
DRyazanov <4PsuUhyz>
2021-11-13 16:40:10 +0300

    hateoas to users in get user request

--- restful-web-services/src/main/java/com/ryazanov/rest/webservices/restfulwebservices/controller/UserController.java
+++ restful-web-services/src/main/java/com/ryazanov/rest/webservices/restfulwebservices/controller/UserController.java
@@ -5,6 +5,8 @@ import com.ryazanov.rest.webservices.restfulwebservices.entity.User;
 import com.ryazanov.rest.webservices.restfulwebservices.exception.UserNotFoundException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +19,9 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @Slf4j
@@ -31,14 +36,20 @@ public class UserController {
     }
 
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id) {
+    public EntityModel<User> retrieveUser(@PathVariable int id) {
         User user = userService.find(id);
 
         if (user == null) {
             throw new UserNotFoundException("id-" + id);
         }
 
-        return user;
+        EntityModel<User> resource = EntityModel.of(user);
+
+        WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+
+        resource.add(linkTo.withRel("all-users"));
+
+        return resource;
     }
 
     @PostMapping("/users")
