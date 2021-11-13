1d1165400e625287e79ade923d72a9d1515d260c
DRyazanov <4PsuUhyz>
2021-11-13 16:22:59 +0300

    create user field validations

--- restful-web-services/src/main/java/com/ryazanov/rest/webservices/restfulwebservices/controller/UserController.java
+++ restful-web-services/src/main/java/com/ryazanov/rest/webservices/restfulwebservices/controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
@@ -41,7 +42,7 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public ResponseEntity<Object> createUser(@RequestBody User user) {
+    public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
         User savedUser = userService.save(user);
 
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
@@ -57,8 +58,6 @@ public class UserController {
         if (user == null) {
             throw new UserNotFoundException("id-" + id);
         }
-
-
     }
 
 }
--- restful-web-services/src/main/java/com/ryazanov/rest/webservices/restfulwebservices/entity/User.java
+++ restful-web-services/src/main/java/com/ryazanov/rest/webservices/restfulwebservices/entity/User.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Data
@@ -11,6 +13,10 @@ import java.util.Date;
 @NoArgsConstructor
 public class User {
     private Integer id;
+
+    @Size(min = 2, message = "Name should have at least 2 characters")
     private String name;
+
+    @Past
     private Date birthDate;
 }
